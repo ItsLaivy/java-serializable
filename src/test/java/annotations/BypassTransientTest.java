2e85cf7775a768118544448a2a5ede1a972d099d
Daniel Richard <dnlfg.contato@gmail.com>
2024-10-13 19:39:52 -0300

    added collection and bypass transient tests

---
+++ src/test/java/annotations/BypassTransientTest.java
@@ -0,0 +1,55 @@
+package annotations;
+
+import codes.laivy.serializable.annotations.BypassTransient;
+import codes.laivy.serializable.json.JsonSerializer;
+import com.google.gson.JsonObject;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BypassTransientTest {
+
+    private static final @NotNull JsonSerializer serializer = new JsonSerializer();
+
+    @Test
+    @DisplayName("Test with transient fields")
+    public void withTransients() {
+        @NotNull TestWithTransient test = new TestWithTransient();
+        @NotNull JsonObject object = serializer.serialize(test).getAsJsonObject();
+        @NotNull JsonObject field = object.getAsJsonObject("field");
+
+        Assertions.assertTrue(field.has("ignoreMe"), "cannot find transient field '" + object + "'");
+    }
+    @Test
+    @DisplayName("Test without transient fields")
+    public void withoutTransients() {
+        @NotNull TestWithoutTransient test = new TestWithoutTransient();
+        @NotNull JsonObject object = serializer.serialize(test).getAsJsonObject();
+        @NotNull JsonObject field = object.getAsJsonObject("field");
+
+        Assertions.assertFalse(field.has("ignoreMe"), "the transient field is here '" + object + "'");
+    }
+
+    // Classes
+
+    private static final class TestWithTransient {
+
+        @BypassTransient
+        private final @NotNull Default field = new Default();
+
+    }
+    private static final class TestWithoutTransient {
+
+        private final @NotNull Default field = new Default();
+
+    }
+
+    private static final class Default {
+
+        private final @NotNull String key = "Laivy is cool.";
+        private final transient @NotNull String ignoreMe = "I'm here!";
+
+    }
+
+}
